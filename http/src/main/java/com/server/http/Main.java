6c66865e1933643bbebf891fbd6bbbe0f4131972
Aidar <aidarsarvartdinov2004@gmail.com>
2026-04-05 18:02:45 +0300

    feat: add endpoints for reading and saving files

--- http/src/main/java/com/server/http/Main.java
+++ http/src/main/java/com/server/http/Main.java
@@ -1,4 +1,4 @@
-package com.server;
+package com.server.http;
 
 import com.server.http.server.Server;
 import com.server.http.server.common.ApplicationParameters;
@@ -6,7 +6,7 @@ import com.server.http.server.common.ApplicationParameters;
 public class Main {
     public static void main(String[] args) {
         ApplicationParameters.getInstance().setFileDirectory(args);
-        
+
         Server server = new Server(4221);
         server.start();
     }
